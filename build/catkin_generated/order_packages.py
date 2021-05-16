007f86c8bff5c10008950a06dd659a016b03f4f2
moneylashe <monitriceturner@gmail.com>
2021-05-15 17:04:43 -0700

    intial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/monitrice/project2_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/monitrice/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/monitrice/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ build/my_simulation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/monitrice/project2_ws/src/my_simulation
+# Build directory: /home/monitrice/project2_ws/build/my_simulation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
